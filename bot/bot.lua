a959b0f7989519862537e0e808a2655e818dd2f9
shayanshojaei1 <shayanshojaei.us@gmail.com>
2016-10-29 11:36:26 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {167134629},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
